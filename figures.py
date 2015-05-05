ec3b129023186e921cc1c49531fafce63b0a31b8
DanielAndreasen <daniel.andreasen@astro.up.pt>
2015-05-05 16:00:14 +0100

    Added code for figure for HD20010 parameters with different EP cuts

--- figures.py
+++ figures.py
@@ -3,13 +3,14 @@
 
 # My imports
 from __future__ import division, print_function
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('white')
-sns.set_context('paper')
-# sns.set_context('poster')
+# sns.set_context('paper')
+sns.set_context('poster')
 from glob import glob
 
 
@@ -114,15 +115,16 @@ def fig_EPcut_sun(fout=None):
 
     T, logg, vt, feh, epcut = parameters.T
     ax1 = plt.subplot(221)
-    _plot_result(data=(epcut, T), ylabel='Teff', solar=solar[0])
+    _plot_result(data=(epcut, T), ylabel=r'$\mathrm{T_{eff}}$', solar=solar[0])
 
     ax2 = plt.subplot(222, sharex=ax1)
     ax2.yaxis.tick_right()
     ax2.yaxis.set_label_position('right')
-    _plot_result(data=(epcut, logg), ylabel='logg', solar=solar[1])
+    _plot_result(data=(epcut, logg), ylabel=r'$\log(g)$', solar=solar[1])
 
     ax3 = plt.subplot(223, sharex=ax1)
-    _plot_result(data=(epcut, vt), xlabel=True, ylabel='vt', solar=solar[2])
+    _plot_result(data=(epcut, vt), xlabel=True, ylabel=r'$\xi_\mathrm{micro}$',
+                 solar=solar[2])
 
     ax4 = plt.subplot(224, sharex=ax1)
     ax4.yaxis.tick_right()
@@ -134,19 +136,91 @@ def fig_EPcut_sun(fout=None):
     plt.show()
 
 
-def fig_something(fout=None):
-    """Figure of something
-
-    :fout: Save file to this
+def fig_HD20010_parameters():
     """
+    Derived parameters for HD20010 at different cuts in EP
+    """
+    def _get_parameters(file):
+        with open(file, 'r') as lines:
+            for _ in range(2):
+                lines.readline()
+            line = lines.readline()
+        z = line.strip().replace(' ', '').split('=')
+        return z
+
+    p = '/home/daniel/Software/SPECPAR/HD20010/'
+    N = len(glob(p+'Out*.moog'))
+    data = np.zeros((N, 5))
+
+    prefixes = (p+'Out_moog_b.', p+'Out_moog_')
+    with open(p+'star_file', 'r') as files:
+        # Dealing with the header.
+        for _ in range(2):
+            files.readline()
+        for index, file in enumerate(files):
+            ep = file.split('.moog')[0].split('_')[-1]
+            ep = float(ep)
+            file = file.split(' ')[0]
+            file1 = prefixes[0] + file
+            file2 = prefixes[1] + file
+            if os.path.isfile(file1):
+                z = _get_parameters(file1)
+            else:
+                z = _get_parameters(file2)
+            try:
+                if ep == 1.0:
+                    ep = 6.2
+                data[index, 0] = ep
+                data[index, 1] = int(z[1][:4])  # Teff
+                data[index, 2] = float(z[2][:4])  # logg
+                data[index, 3] = float(z[3][:4])  # microturbulence
+                data[index, 4] = float(z[-1])  # metalicity
+            except ValueError:
+                data[index, 0] = ep
+                data[index, 1] = int(z[1][:4])  # Teff
+                data[index, 2] = float(z[2][:4])  # logg
+                data[index, 3] = np.nan  # microturbulence
+                data[index, 4] = float(z[-1])  # metalicity
+
+    df = pd.DataFrame({'EP':   data[0:-1, 0],
+                       'Teff': data[0:-1, 1],
+                       'logg': data[0:-1, 2],
+                       'vt':   data[0:-1, 3],
+                       'feh':  data[0:-1, 4]})
+
+
+    c = sns.color_palette()
+    f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.setp(ax3, xticks=np.arange(5, 6.2, 0.1),
+             xticklabels=list(np.arange(5, 6.1, 0.1)) + ['No cut'])
+
+    sns.regplot('EP', 'Teff', df, truncate=True, order=2, ax=ax1).set_xlabel('')
+    ax1.plot(6.1, data[-1, 1], 'o', color=c[0], ms=6)
+    ax1.set_ylabel(r'$\mathrm{T_{eff}}$ [K]')
+
+    sns.regplot('EP', 'feh', df, truncate=True, order=2, ax=ax2).set_xlabel('')
+    ax2.plot(6.1, data[-1, 4], 'o', color=c[1], ms=6)
+    ax2.set_ylabel('[Fe/H]')
+
+    sns.regplot('EP', 'vt', df, truncate=True, order=2, ax=ax3)
+    ax3.plot(6.1, data[-1, 3], 'o', color=c[2], ms=6)
+    ax3.set_ylabel(r'$\xi_\mathrm{micro}$ [km/s]')
+
+    plt.axis((4.95, 6.15, -0.5, 3.3))
+    plt.show()
+
+    # plt.savefig('figures/HD20010_parameters_cuts.pdf')
 
 
 def main():
     """Main function
     :returns: TODO
     """
-    fig_abundance()
+    # fig_abundance()
     # fig_EPcut_sun()
+    fig_HD20010_parameters()
 
 
 if __name__ == '__main__':
